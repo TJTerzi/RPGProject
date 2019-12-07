dfb1b2954fe3b6c644e3851fa3c66891f413e1e0
Abias24 <53461223+Abias24@users.noreply.github.com>
2019-12-07 15:51:52 +1100

    Preparing to add a new scene

---
+++ RPG PRoject/Assets/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsControlRemover : MonoBehaviour
+    {
+
+        GameObject player;
+        
+        
+        private void Start() 
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+            GameObject player = GameObject.FindWithTag("Player");
+        }
+
+        void DisableControl(PlayableDirector playableDirector)
+        {
+           
+           player.GetComponent<ActionScheduler>().CancelCurrentAction();
+           player.GetComponent<PlayerController>().enabled = false; 
+        }
+
+        void EnableControl(PlayableDirector playableDirector)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+}
